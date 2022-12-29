4a4e71515602123f379f02431dc2db4083229ea0
YuBeen-Park <park_ubin@naver.com>
2022-12-29 11:21:39 +0900

    [chore] #5 SearchFragment 중괄호 삭제

--- app/src/main/java/org/sopt/sample/presentation/home/SearchFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/SearchFragment.kt
@@ -4,6 +4,4 @@ import org.sopt.sample.R
 import org.sopt.sample.base.BindingFragment
 import org.sopt.sample.databinding.FragmentSearchBinding
 
-class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
-
-}
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search)
